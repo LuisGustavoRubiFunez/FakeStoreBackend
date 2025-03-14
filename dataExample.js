e25dc819443cfe95406516402b8854e9b83d8543
LuisGustavoRubi <161526218+LuisGustavoRubi@users.noreply.github.com>
2025-03-13 18:41:34 -0600

    Update dataExample.js

--- dataExample.js
+++ dataExample.js
@@ -1,21 +1,21 @@
 export const productos=[
     {
         nombre:"Coca cola",
-        imgRuta:"http://localhost:3000/static/cocaCola2L.png",
+        imgRuta:"https://fakestorebackend.onrender.com/static/cocaCola2L.png",
         descripcion:"Bebida azucarada de la marca coca ",
         precio:55,
         tipoMoneda:"L."
     },
     {
         nombre:"Iphone 15",
-        imgRuta:"http://localhost:3000/static/iphone15.png",
+        imgRuta:"https://fakestorebackend.onrender.com/static/iphone15.png",
         descripcion:"Telefono gama alta de marca Apple",
         precio:"800",
         tipoMoneda:"$"
     },
     {
         nombre:"Balon de baloncesto",
-        imgRuta:"http://localhost:3000/static/balonBaloncesto.png",
+        imgRuta:"https://fakestorebackend.onrender.com/static/balonBaloncesto.png",
         descripcion:"Balon profesional de baloncesto",
         precio:500,
         tipoMoneda:"L."
